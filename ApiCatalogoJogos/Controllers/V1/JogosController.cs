20257e5cba3addd9e08459e77a63bf7c06c9b994
Victor Torres <vstorresti@gmail.com>
2021-06-12 01:03:01 -0300

    First Commit

---
+++ ApiCatalogoJogos/Controllers/V1/JogosController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalogoJogos.Controllers.V1
+{
+    [Route("api/v1/[controller")]
+    [ApiController]
+    public class JogosController : ControllerBase
+    {
+
+    }
+}
